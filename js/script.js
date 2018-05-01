f65db629682f97c9e64c3bc8f6f9b45069f9531e
Eddie <eddie.jostell@gmail.com>
2018-05-01 18:03:12 +0200

    some progress

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 const clanModule = (() => {
+  return {
+    pageScrollFunction: () => {
+      $(document).ready(function() {
+        $(".nav-link").on("click", function() {
+          $.scrollTo($(this).attr("href"), 800, {
+            offset: -60
+          });
+        });
+      });
+    },
 
-    return {
+    hideCollapsedNavbar: () => {
+      $(document).on("click", function() {
+        $(".collapse").collapse("hide");
+      });
+    },
 
-        pageScrollFunction:() => {
-			$(document).ready(function() {
-				$('.nav-link').on('click', function() {
-					$.scrollTo($(this).attr('href'), 800, {
-						offset: -60
-					});
-				});
-			});
-        },
-        
-        hideCollapsedNavbar: () => {
-			$(document).on('click',function(){
-				$('.collapse').collapse('hide');
-			})
-        },
+    imageSlider: () => {
+    
+    },
 
-        registerEventHandlers: function() {
-			
-		},
-        
-        initialize: (() => {
-			document.addEventListener('DOMContentLoaded', () => {
-				clanModule.pageScrollFunction();
-				clanModule.registerEventHandlers();
-				clanModule.hideCollapsedNavbar();
-				
-			});
-		})()
-    }
+    registerEventHandlers: function() {},
+
+    initialize: (() => {
+      document.addEventListener("DOMContentLoaded", () => {
+        clanModule.pageScrollFunction();
+        clanModule.registerEventHandlers();
+        clanModule.hideCollapsedNavbar();
+        clanModule.imageSlider();
+      });
+    })()
+  };
 })();
